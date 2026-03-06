71305e035ca94af416a3148c13f2a85068ebc78c
Pedro Lemos <pedrolemos906@gmail.com>
2026-03-06 16:32:40 -0300

    commit de hoje

---
+++ 1 Semestre/Algoritmos e Técnicas de Progamação/IdentificarAno/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO.Pipes;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Identificador do Ano!");
+        Console.Write("Informe o ano [XXXX]: ");
+
+        int ano = int.Parse(Console.ReadLine());
+
+        int regra1 = ano % 400;
+
+        int regra2 = ano % 4;
+
+        if (regra1 == 0) Console.Write(ano + " não é um ano Bissexto");
+
+            if (regra2 == 0) Console.Write(ano + " não é um ano Bissexto");
+            
+        
+        else
+        { 
+            Console.Write(ano + " não é um ano Bissexto");
+        }
+
+
+
+    }
+}
