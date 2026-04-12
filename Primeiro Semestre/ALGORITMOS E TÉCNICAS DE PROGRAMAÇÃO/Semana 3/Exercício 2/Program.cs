0e8d8e97e77d7e7f73660fd709fa1a15e48f3a21
Pedro Lemos <pedrolemos906@gmail.com>
2026-04-12 14:55:59 -0300

    commit de hoje - desktop

---
+++ Primeiro Semestre/ALGORITMOS E TÉCNICAS DE PROGRAMAÇÃO/Semana 3/Exercício 2/Program.cs
@@ -0,0 +1,24 @@
+﻿//Faça um programa que realize as quatro operaçoes básicas. 
+using System;
+class Progam
+{
+    static void Main()
+    {
+        double a, b;
+        char op;
+
+        Console.WriteLine("esse programa é uma calculadora simples.");
+        Console.WriteLine("informe o primerio número: ");
+        a = double.Parse(Console.ReadLine());
+        Console.WriteLine("informe o segundo número: ");
+        b = double.Parse(Console.ReadLine());
+        Console.WriteLine("informe a operação que deseja fazer [+, -, *, /]: ");
+        
+        
+
+
+        Console.WriteLine($"{posicao1}, {posicao2}, {posicao3}");
+
+    }
+}
+
