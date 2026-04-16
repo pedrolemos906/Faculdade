671763f349d4e1c92bcaf4d658a517fb1de125d7
Pedro Lemos <pedrolemos906@gmail.com>
2026-04-16 18:33:00 -0300

    commit de hoje - notebook

---
+++ Primeiro Semestre/ALGORITMOS E TÉCNICAS DE PROGRAMAÇÃO/Semana 8/Exercício/Program.cs
@@ -0,0 +1,26 @@
+﻿//Faça umprograma que receba as três notas, calcule e mostre a média ponderada e o conceito que segue a tabela:
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int numero, soma;
+
+        Console.WriteLine("esse programa mostra a soma dos numeros informados.");
+        Console.WriteLine("digite um valor; ");
+
+        numero = int.Parse(Console.ReadLine());
+        
+        while (numero != 0)
+        {
+            soma = soma + numero;
+            Console.WriteLine("digite outro valor: ");
+            numero = int.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine($"soma: {soma}");
+
+       
+    }
+}
