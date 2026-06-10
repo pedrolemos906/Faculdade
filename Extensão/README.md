c6236595f84d968f8c61e1669b59ec77fc7b9664
Pedro Lemos <pedrolemos906@gmail.com>
2026-06-10 09:13:08 -0300

    commit de hoje

---
+++ Extensão/README.md
@@ -0,0 +1,17 @@
+11/05/2026
+	reuniao de alinhamento com o Professor Victor. O professor quer que eu atue na organização e no armazenamento dos arquivos resultantes dos trabalhos de extensao. 
+
+
+		professores: luciana, maria ines, ivan
+		disciplinas: 
+			si
+				metodologia de extensao
+				trabalho interdisciplinar para aplicaçoes me processos de negocios
+				trabalho interdisciplinar para aplicaçoes para sustentabilidade
+			ads
+				trabalho interdisciplinar para aplicacoes mobile 
+
+
+		Canvas/pucvirtual 
+		felipe antunes guimaraes
+			guimaraes.antunes@puc.com.br
--- Primeiro Semestre/ALGORITMOS E TÉCNICAS DE PROGRAMAÇÃO/Semana 2/Exercício 4/obj/Debug/net10.0/Exercício1.AssemblyInfo.cs
+++ Primeiro Semestre/ALGORITMOS E TÉCNICAS DE PROGRAMAÇÃO/Semana 2/Exercício 4/obj/Debug/net10.0/Exercício1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exercício1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebdcb74d36e228c7fabf0828fe38e577e8969b0d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2533c064dbdf45697ef55e681f58eb1ab3f3b7df")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exercício1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exercício1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
